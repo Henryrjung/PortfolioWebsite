1eb816912b83708000abf4780445f32319e345d5
HenryJ <henryjung667@gmail.com>
2022-03-17 16:53:17 -0500

    updated style

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -5,15 +5,30 @@ export default function About() {
   return <div className='about' id='about'>
     <Nav />
       <div className="left">
-        <div className="imgCont">
-          <img src="assets/homepagebackground copy.png" alt="" />
+        <div className="containerTop">
+          <div className="imgCont">
+          <img src="assets/profile pic copy 2.png" alt="" />
         </div>
-       
+        <div className="topText">
+          {/* <h1>Hello</h1>
+          <h2>I'm</h2> */}
+        </div>
+        </div>
+        <div className="containerBottom">
+          <h2>Hello I'm</h2>
+          <h1>Henry Jung</h1>
+          <h2>Freelance Front End</h2>
+          <h3>Developer</h3>
+        </div>
       </div> 
       <div className="right">
-          <h2>Hello, I'm</h2>
-          <h1>Henry Jung</h1>
-          <h3>Freelance front End Developer</h3>
+        <div className="topSection">
+          <h1>About Me</h1>
+        </div>
+        <div className="bottomSection">
+          <h3>I'm a freelance front end developer based out of Kansas City and New York. I Have a certificate from KU university in full stack development and work mainly in React, CSS, and Javascript.</h3>
+        </div>
+          
         </div>
   </div>;
 }
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,10 +2,7 @@ import './portfolio.scss';
 
 export default function Portfolio() {
   return <div className='portfolio' id='portfolio'> 
-        <div className="leftPortfolio">
-        <h1>Portfolio</h1> 
-        </div>
-      <div className="rightPortfolio">
+      <div className="portfolioBox">
         <div className="item">
               <img src="https://picsum.photos/640/360
                         " alt=""/>
